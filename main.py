ddbb4b2a0e219dc0e1a88d3f368039abfa588b3b
IldarionStarstrider <harm.denhertog@hotmail.com>
2024-12-10 20:42:59 +0100

    changed sort_by, sort_list, expand_dict, main and create_report in order to finalize create_report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+def main(directory):
+    with open(directory) as f:
         file_contents = f.read()
         return file_contents
 
@@ -11,42 +11,49 @@ def wordcounter(string):
         count += 1
     return count
 
-#counts the number of times a character is used in the text.
+#counts the number of times a character is used in the text and delivers an alphabetized dictionary.
 def charcounter(string):
     lowered_string = string.lower()
     dict = {}
     for character in lowered_string:
         if character in dict:
             dict[character] += 1
-        else:
+        elif character.isalpha():
             dict[character] = 1
     return dict
 
-#converts the charcounter dictionary to a list of dictionaries.
-def convert_dict(dict):
+#cexpand the charcounter dictionary with char and num keys and adds them to a list.
+def expand_dict(dict):
     new_list = []
-    for k, v in dict:
-        
-    print(new_list)
+    for k in dict:
+        expanded_dict = {}
+        expanded_dict["char"] = k
+        expanded_dict["num"] = dict[k]
+        new_list.append(expanded_dict)
+    return new_list
 
+#tells what to sort by
+def sort_by(dict):
+    return dict["num"]
 
+#sorts the list from convert_dict on number of times.
+def sort_list(list):
+    list.sort(reverse=True, key=sort_by)
+    return list
 
-
-
-#creates a report of frankenstein.txt
-#def create_report(string):
-    print("--- Begin report of books/frankenstein.txt ---")
+#creates a report of directory
+def create_report(directory):
+    string = main(directory)
+    print(f"--- Begin report of {directory} ---")
     print(f"{wordcounter(string)} words found in the document")
+    print("")
+    print("")
     dict = charcounter(string)
-    for key in dict:
-        print(f"The {key} character was found {dict[key]} times")
+    list = expand_dict(dict)
+    list = sort_list(list)
+    for d in list:
+        print(f"The '{d["char"]}' character was found {d["num"]} times")
+    print("--- End report ---")
 
 
-
-
-
-
-
-convert_dict(charcounter(main()))
-
-#create_report(main())
+create_report("books/frankenstein.txt")
